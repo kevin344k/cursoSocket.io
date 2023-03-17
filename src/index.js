2c6a9620a33d4ad0f0642540c76c0344b851871f
kevin344k <kevin344k@gmail.com>
2023-03-17 07:43:30 +0000

    hola

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express=require("express")
+const {createServer}=require("http")
+const {Server}=require("socket.io")
+const path=require("path")
+
+
+
+const app=express()
+const httpServer=createServer(app)
+const io=new Server(httpServer)
+
+
+app.use(express.static(path.join(__dirname,"views")))
+
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname+"views/index.html")
+})
+
+
+io.on("connection",socket=>{
+  console.log(socket)
+})
+
+
+httpServer.listen(3000)
