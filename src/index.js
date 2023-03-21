ef4e88d3a1c956a3b7108a7b170367b16dd2a4a0
kevin344k <kevin344k@gmail.com>
2023-03-21 19:20:33 +0000

    sd

--- src/index.js
+++ src/index.js
@@ -21,60 +21,7 @@ app.get("/",(req,res)=>{
 io.on("connection",socket=>{
 //Broadcast de eventos
 
-  socket.on("circle position",position=>{
-    socket.broadcast.emit("move circle",position)
-  })
-  
 
-  /*
- // console.log(socket.id)
- // console.log("clientes conectdos :", io.engine.clientsCount)
-
-/*socket.on("disconnect",()=>{
-  
-  console.log("El socket "+ socket.id+ "se ha desconectado")
-})
-socket.conn.once("upgrade",()=>{
-  console.log("hemos pasado de HTTP Long-Polling a ",socket.conn.transport.name)
-})
-
-
-//emision basica
-
-  socketsOnline.push(socket.id)
-  console.log(socketsOnline)
-  socket.emit("welcome","Ahora estas conectado 😎.")
-
-socket.on("server",data=>{
-  console.log(data)
-})
-
-  //emision a todos
-
-  io.emit("everyone","El cliente : "+socket.id+"se ha conectado 👀")
-
-  //emision a uno solo
-
-  socket.on("last",message=>{
-    const lastSocket=socketsOnline[socketsOnline.length-1]
-
-    io.to(lastSocket).emit("salute",message)
-    
-  })
-
-
-  //on,once y off
-  /*socket.emit("on","hola")
-    socket.emit("on","hola")
-socket.emit("once","hola")
-  socket.emit("once","hola")
-  
-socket.emit("off","hola")
-
-  setTimeout(()=>{
-    socket.emit("off","hola")
-  },3000)
-  */
 })
 
 
